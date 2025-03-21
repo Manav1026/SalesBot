376afaa890f7bba34a6f436bcac4937ceea09a32
Manav1026 <manav.dhami10@gmail.com>
2025-03-21 15:22:13 -0400

    updated page.tsx

---
+++ salesbot/src/app/results/page.tsx
@@ -0,0 +1,12 @@
+import ResultsDisplay from "@/components/ResultDisplay";
+
+export default function ResultsPage() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-center mt-5">
+        SalesBot Interview Results
+      </h1>
+      <ResultsDisplay />
+    </div>
+  );
+}
